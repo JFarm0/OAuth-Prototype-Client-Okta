5c90473e18d4c901cbd45b4df061f4008c2d4c02
JFarm0 <79594115+JFarm0@users.noreply.github.com>
2021-08-22 21:32:50 -0500

    Initial Commit

---
+++ src/test/java/com/okta/spring/OktaOAuthClient/OktaOAuthClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.okta.spring.OktaOAuthClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OktaOAuthClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
